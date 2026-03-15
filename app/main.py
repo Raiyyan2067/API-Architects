0b4283df8ac931df2b2267dff2dd2f89ab4ae4ad
Raiyyan <z5692476@ad.unsw.edu.au>
2026-03-16 00:23:55 +1100

    Refactor FastAPI initialization to include title and version

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from app.api.v1 import despatch, health
+from mangum import Mangum
 
-app = FastAPI()
-
+app = FastAPI(title="UBL Despatch API", version="2.1")
 
 @app.get("/")
 def root():
     return {"message": "UBL API is running"}
 
-
 app.include_router(despatch.router, prefix="/ubl/v2/despatch-advice")
 app.include_router(health.router, prefix="/ubl/v2/despatch-advice")
+
+handler = Mangum(app)
